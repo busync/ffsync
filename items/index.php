4f9ee2b86f230e6d6bf017266a2b3ff08f4aa546
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-06-10 04:13:33 +0400

    update: added light switch button and added cursor

--- items/index.php
+++ items/index.php
@@ -48,6 +48,9 @@ $userData = $userController->getUserData();
 
                 <div class="right--top--panel--system">
                     <button onclick="window.location = ''">
+                        <img src="../source/imges/sun.png" alt="">
+                    </button>
+                    <button onclick="window.location = ''">
                         <img src="../source/imges/person.png" alt="">
                     </button>
                 </div>
